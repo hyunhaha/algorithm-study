4e9316040cc4e530371d2d51e867c97910c6cef4
hyunhaha <gusgk2658@naver.com>
2021-02-17 18:15:05 +0900

    섬연결하기 다른 풀이

--- 프로그래머스/3단계/섬연결하기.js
+++ 프로그래머스/3단계/섬연결하기.js
@@ -26,3 +26,37 @@ function solution(n, costs) {
 
   return answer;
 }
+
+//Find-Union 알고리즘 사용
+function solution(n, costs) {
+  var answer = 0;
+  let table = new Array(n).fill(0).map((e, idx) => idx);
+  costs.sort((a, b) => a[2] - b[2]);
+
+  const isAllSame = (table) => {
+    for (let i = 1; i < table.length; i++) {
+      if (table[i] !== table[i - 1]) return false;
+    }
+    return true;
+  };
+
+  const changeTable = (start, end, table) => {
+    let tableEnd = table[end];
+    let tableStart = table[start];
+    for (let i = 0; i < table.length; i++) {
+      if (table[i] === tableEnd) {
+        table[i] = tableStart;
+      }
+    }
+    return table;
+  };
+
+  while (!isAllSame(table)) {
+    let [start, end, cost] = costs.shift();
+    if (table[start] !== table[end]) {
+      table = changeTable(start, end, table);
+      answer += cost;
+    }
+  }
+  return answer;
+}

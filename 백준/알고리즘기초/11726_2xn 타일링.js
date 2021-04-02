bdb78f859a1b3c26578fc95bc238ffca556ac106
hyunhaha <gusgk2658@naver.com>
2021-04-02 23:17:37 +0900

    다이나믹 프로그래밍

--- 백준/알고리즘기초/11726_2xn 타일링.js
+++ 백준/알고리즘기초/11726_2xn 타일링.js
@@ -1,15 +1,10 @@
 const solution = (line) => {
-  let arr = [];
+  let arr = new Array(line + 1).fill(0);
   arr[0] = 0;
-  arr[1] = 0;
-  for (let i = 2; i <= line; i++) {
-    arr[i] = arr[i - 1] + 1;
-    if (i % 3 === 0) {
-      arr[i] = Math.min(arr[i], arr[i / 3] + 1)
-    }
-    if (i % 2 === 0) {
-      arr[i] = Math.min(arr[i], arr[i / 2] + 1)
-    }
+  arr[1] = 1;
+  arr[2] = 2;
+  for (let i = 3; i <= line; i++) {
+    arr[i] = (arr[i - 1] + arr[i - 2]) % 10007
   }
   return arr[line]
 }

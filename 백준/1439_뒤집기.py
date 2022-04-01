6d5192f5805c6255eb6515f85ad0588e3d9ce7d8
hyunhaha <gusgk2658@naver.com>
2022-04-01 23:37:24 +0900

    뒤집기

---
+++ 백준/1439_뒤집기.py
@@ -0,0 +1,15 @@
+data=input()
+zero=0
+one=0
+
+if data[0]=='0':
+    one+=1
+else:
+    zero+=1
+for i in range(len(data)-1):
+    if data[i]!=data[i+1]:
+        if data[i+1]=='0':
+            one+=1
+        else:
+            zero+=1
+print(min(zero,one)) 
--- 백준/tempCodeRunnerFile.js
+++
@@ -1,1 +0,0 @@
-seconed for
